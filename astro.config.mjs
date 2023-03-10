5f6255d2efe038fa4fc1fd4922758825bd4a151b
Shakhruz Rakhmatov <iamrakhmatov@gmail.com>
2023-03-10 09:25:24 +0500

    fix: refactor react to astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-import mdx from "@astrojs/mdx";
-
-// https://astro.build/config
-import solidJs from "@astrojs/solid-js";
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), solidJs()]
+  integrations: [tailwind(), react()]
 });
